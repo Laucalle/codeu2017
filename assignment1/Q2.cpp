c800ffb39ddf9a5ca9e763465da0df502b276149
Laura Calle Caraballo <laucalle09@correo.ugr.es>
2017-05-19 03:49:30 +0200

    Update Q2 after comments

--- assignment1/Q2.cpp
+++ assignment1/Q2.cpp
@@ -8,22 +8,24 @@ struct cell{
 };
 
 // precondition: init is a single linked list, NULL terminated
-// If k is greater than the size of the list, a NULL pointer is 
-// returned, indicating that kth element to last does not exist
+// If k is greater or equal to the size of the list, a NULL 
+// pointer is returned, indicating that kth element to last 
+// does not exist
 // If k is negative (invalid input), a NULL pointer is returned
 template <typename T>
 cell<T>* kToLast(cell<T>* init, int k){
 
-	cell<T>* advanced, *out;
+	cell<T>* advanced, *kth;
 	advanced = init;
 	for(int i=0; i<=k && advanced!=NULL; i++){
 		advanced = advanced->next;
 	}
-	if(advanced==NULL) return advanced;
-	out = init;
+	if(advanced == NULL && i-1 == k) return init;
+	else if(advanced==NULL) return advanced;
+	kth = init;
 	while( advanced != NULL){
 		advanced = advanced->next;
-		out = out->next;
+		kth = kth->next;
 	}
-	return out;
+	return kth;
 }
