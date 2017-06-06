88664fd8d034d9cdf2ae0f7c6d778a0f4635e621
Laura Calle Caraballo <laucalle09@correo.ugr.es>
2017-06-06 11:04:04 +0200

    Added changes suggested in comments

--- assignment2/assignment2.cpp
+++ assignment2/assignment2.cpp
@@ -5,17 +5,17 @@
 template <typename T>
 class BinaryTree
 {
-public:
+private:
 	struct Node{
 		T data;
 		Node *left_child;
 		Node *right_child;
 	};
-private:
+
 	Node *root;
 	
 	void remove_subtree(Node * node){
-		if(node == NULL){ return; }
+		if(node == nullptr){ return; }
 		Node *left= node->left_child;
 		Node *right = node->right_child;
 		delete node;
@@ -23,10 +23,10 @@ private:
 		remove_subtree(right);
 	}
 
-	bool insert(const T &key, Node *tree, const T &new_data, bool right){
-		if(tree != NULL){
+	bool insert(const T &parent_key, Node *tree, const T &new_data, bool right){
+		if(tree != nullptr){
 
-			if(tree->data == key){
+			if(tree->data == parent_key){
 				if(right){
 					tree->right_child = new Node;
 					tree->right_child->data = new_data;
@@ -36,8 +36,8 @@ private:
 				}
 				return true;
 			}
-			if(insert(key, tree->left_child, new_data, right) || 
-				insert( key, tree->right_child, new_data, right)){
+			if(insert(parent_key, tree->left_child, new_data, right) || 
+				insert( parent_key, tree->right_child, new_data, right)){
 				return true;
 			}
 		}
@@ -45,15 +45,16 @@ private:
 	}
 
 	// Answer to Q1 from assignment2
-	bool print_ancestors(const T &key, Node *tree, std::vector<T> &ancestors){
-		if(tree != NULL){
+	// Ancestors to key are saved into ancestors vector
+	bool print_ancestors(const T &key, Node *tree, std::vector<T>* ancestors){
+		if(tree != nullptr){
 
 			if(tree->data == key){
 				return true;
 			}
 			if(print_ancestors(key, tree->left_child, ancestors) || 
 				print_ancestors( key, tree->right_child, ancestors)){
-				ancestors.push_back(tree->data);
+				ancestors->push_back(tree->data);
 				return true;
 			}
 		}
@@ -61,10 +62,13 @@ private:
 	}
 
 	// Answer to Q2 from assignment2
+	// Returns lowest common ancestor of val1 and val2, given keys are unique 
+	// and val1 != val2(in which case nullptr is returned.
+	// If one of them is absent, it will return a nullptr
 	Node* common_ancestor(const T& val1, const T&val2,Node* tree, bool& found){
-		if(tree == NULL){
+		if(tree == nullptr){
 			found = false;
-			return NULL;
+			return nullptr;
 		}
 		bool left = false;
 		bool right = false;
@@ -74,10 +78,10 @@ private:
 			found = right || left;
 			if(found) return tree;
 			found = true;
-			return NULL;
+			return nullptr;
 		}
 		Node* common = common_ancestor(val1, val2, tree->right_child, right); 
-		if(common == NULL){
+		if(common == nullptr){
 			common = common_ancestor(val1, val2, tree->left_child, left);
 		}
 		found = right || left;
@@ -86,34 +90,95 @@ private:
 	}
 public:
 	BinaryTree(){
-		root = NULL; 
+		root = nullptr; 
 	}
 	~BinaryTree(){
-		if(root != NULL){
-			remove_subtree(root);
-		}
+		remove_subtree(root);
 	}
 
+	// Set root's value
 	void setRoot(const T &key){
-		if(root == NULL)
+		if(root == nullptr)
 			root = new Node;
 		root->data=key;
 	}
 
-	bool Insert(const T &key, const T &new_data, bool right){
-		return insert(key, root, new_data, right);
+	// Inserts new_data as a child of parent_key.
+	bool Insert(const T &parent_key, const T &new_data, bool right){
+		return insert(parent_key, root, new_data, right);
 	}
 
 	// Wrapper for print_ancestors for Q1
-	bool PrintAncestors(const T &key, std::vector<T> &ancestors){
+	// Saves ancestors to key in ancestors vector (reverse order)
+	bool PrintAncestors(const T &key, std::vector<T> *ancestors){
 		return print_ancestors(key, root, ancestors);
 	}
 
 	// Wrapper for common_ancestors for Q2
-	Node * CommonAncestor(const T& val1, const T&val2){
+	// Returns pointer to data of the lowest common ancestor of val1 and val2,
+	// given that all keys are unique and val1 != val2. If one of them is
+	// not in the tree, nullptr is returned.
+	T * CommonAncestor(const T& val1, const T&val2){
+		if( val1 == val2) return nullptr;
 		bool found = false;
-		return common_ancestor(val1, val2, root, found); 
+		Node * common = common_ancestor(val1, val2, root, found); 
+		if(common != nullptr) return &(common->data);
+		return nullptr;
 	}
 	
 };
 
+int main(){
+	BinaryTree<int> tree;
+
+	tree.setRoot(16);
+	tree.Insert(16, 9, false);
+	tree.Insert(9, 3, false);
+	tree.Insert(3, 1, false);
+	tree.Insert(3, 5, true);
+	tree.Insert(9, 14, true);
+	tree.Insert(16, 18, true);
+	tree.Insert(16, 19, true);
+
+	int* a = tree.CommonAncestor(5,14);
+	if(a == nullptr){
+		std::cerr << "CommonAncestor: error\n";
+	}
+	else if (*a == 9) std::cout << "CommonAncestor: correct\n";
+	else std::cerr << "CommonAncestor: wrong value\n"; 
+
+	// one of the keys is not in tree
+	int* b = tree.CommonAncestor(5,4);
+	if(b == nullptr){
+		std::cout << "CommonAncestor: correct\n";
+	}
+	else std::cerr << "CommonAncestor: error\n";
+
+	// one of the keys is ancestor to the other
+	int* c = tree.CommonAncestor(9,3);
+	if(c == nullptr){
+		std::cerr << "CommonAncestor: error\n";
+	}
+	else if(*c == 9) {
+		std::cout << "CommonAncestor: correct\n";
+	}
+	
+	std::vector<int> ancestors;
+	std::vector<int> test_solution({3,9,16});
+	tree.PrintAncestors(5, &ancestors);
+
+	bool correct = true;
+	if(ancestors.size() == test_solution.size()){
+		for(int i = 0; i<ancestors.size() && correct; i++){
+			correct = ancestors[i] == test_solution[i];
+		}
+	} else correct = false;
+	if(correct) std::cout << "PrintAncestors: correct\n";
+	else std::cerr << "PrintAncestors: error\n";
+
+	ancestors.clear();
+	tree.PrintAncestors(2, &ancestors);
+	if(ancestors.empty()) std::cout << "PrintAncestors: correct\n";
+	else std::cerr << "PrintAncestors: error\n";
+
+}
