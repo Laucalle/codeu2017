876400aa38381f4825ceb15035a1b4ea4f9b7e12
Laura Calle Caraballo <laucalle09@correo.ugr.es>
2017-06-30 14:15:05 +0200

    Update assignment4.cpp

--- assignment4/assignment4.cpp
+++ assignment4/assignment4.cpp
@@ -2,26 +2,40 @@
 #include <math.h>
 #include <vector>
 
-class IslandCounter{
-	void collapse_island(std::vector< std::vector<bool> > &map, int i, int j){
-		if(!map[i][j]) return;
+class IslandCounter {
+	std::vector< std::vector<bool> >  map;
+	void collapse_island ( int i, int j) {
+		if (!map[i][j]) return;
 
 		map[i][j] = false;
-		collapse_island( map, std::min(i+1, (int) (map.size()-1)), j);
-		collapse_island( map, std::max(i-1, 0), j);
-		collapse_island( map, i, std::min(j+1,(int) (map[i].size()-1)));
-		collapse_island( map, i, std::max(j-1,0));
-		
+		int end_row = std::min(i+1, (int) (map.size()-1));
+		int init_row = std::max(i-1, 0);
+		int end_col = std::min(j+1,(int) (map[i].size()-1));
+		int init_col = std::max(j-1,0);
+
+		for(int row = init_row; row <= end_row; row++) {
+			for(int col = init_col; col <= end_col; col++) {
+				if ((col == j || row == i) && !(col==j && row==i)) {
+					collapse_island (row, col);
+				}
+			}
+		}
 		return;
 	}
 public:
-	int Count(std::vector< std::vector<bool> > map){
+	void SetMap(const std::vector< std::vector<bool> > &island_map){
+		map=island_map;
+	}
+	int Count () {
 		int island_counter = 0;
-		for( int i=0; i<map.size(); i++) {
-			for (int j = 0; j < map[i].size(); j++) {
-				if( map[i][j]){
-					island_counter++;
-					collapse_island( map, i, j);
+		
+		if(!map.empty()){
+			for( int i = 0; i < map.size(); i++) {
+				for (int j = 0; j < map[i].size(); j++) {
+					if (map[i][j]) {
+						island_counter++;
+						collapse_island (i, j);
+					}
 				}
 			}
 		}
@@ -42,13 +56,14 @@ int main(){
 										 {0,0,0,0,1,0,1,0,1,0}};
 
 
-	for(int i=0; i<map.size(); i++){
-		for (int j = 0; j<map[i].size(); j++){
+	for(int i = 0; i < map.size(); i++) {
+		for (int j = 0; j < map[i].size(); j++) {
 			if(map[i][j]) std::cout << "x";
 			else std::cout << " "; 
 		}
 		std::cout << std::endl;
 	}
 	IslandCounter ic;
-	std::cout << ic.Count(map) << std::endl;
+	ic.SetMap(map);
+	std::cout << ic.Count() << std::endl;
 }
